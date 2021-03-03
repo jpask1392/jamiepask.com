f0fa5460ae41841491cdb972a43f0fd713645fa5
Jamie Pask <jamiepask1392@gmail.com>
2021-03-02 19:50:34 -0800

    correctly size home images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   trailingSlash: true,
   images: {
-    loader: 'cloudinary'
+    loader: 'cloudinary',
+    deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   }
 };
