9c83d3c17a7312da8ed0b3e7ed91803a1b701aa5
jpask1392 <jamiepask1392@gmail.com>
2021-06-23 08:29:07 -0700

    add moonjuice

--- data/case-studies/index.js
+++ data/case-studies/index.js
@@ -6,5 +6,6 @@ import carkella from 'data/case-studies/carkella';
 import loveleo from 'data/case-studies/loveleo';
 import portfolio from 'data/case-studies/portfolio';
 import coolTrick from 'data/case-studies/coolTrick';
+import moonjuice from 'data/case-studies/moonjuice';
 
-export default [ mutesix, techtonic, houseConcepts, ilola, carkella, loveleo, portfolio, coolTrick ]
+export default [ mutesix, techtonic, houseConcepts, ilola, carkella, loveleo, portfolio, coolTrick, moonjuice ]
